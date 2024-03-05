c05a30ce5d0e8ca2629124aade1bec19df1364f6
eduardovalias <edupv123@hotmail.com>
2024-03-04 23:50:03 -0300

    Add Aula1

---
+++ Laboratorio/Aula1/App.java
@@ -0,0 +1,8 @@
+package Laboratorio.Aula1;
+
+public class App {
+    public static void main(String[] args){
+        System.out.println("Minha primeira aula de POO com Java.");
+        System.out.println("Estamos usando o Netbeans!");
+    }
+}
